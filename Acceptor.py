240e39621cf798793e0091f8dff8cf1fd8bc72b6
djacint <djacint@yahoo.com>
2015-02-18 07:17:39 +0000

    Signed-off-by: djacint <djacint@yahoo.com>

--- Acceptor.py
+++ Acceptor.py
@@ -1,22 +1,19 @@
 import quickfix
+import sys
 
+def main(fileName):
+	if len(sys.argv) < 2: 
+		return
+	fileName = sys.argv[1]
 
-def nameOfTheFile(fileName):
-    print fileName
-
-if len(sys.argv) < 2
-return
-
-fileName = sys.argv[1]
-
-try:
-        settings = quickfix.SessionSettings(fileName)
-        application = quickfix.MyApplication()
-        storeFactory = quickfix.FileStoreFactory(settings)
-        logFactory = quickfix.FileLogFactory(settings)
-        acceptor = quickfix.SocketAcceptor(application, storeFactory, settings, logFactory)
-        acceptor.start()
-        # while condition == true: do something
-        acceptor.stop()
-except quickfix.ConfigError, e:
-        print e
+	try:
+			settings = quickfix.SessionSettings(fileName)
+			application = quickfix.MyApplication()
+			storeFactory = quickfix.FileStoreFactory(settings)
+			logFactory = quickfix.FileLogFactory(settings)
+			acceptor = quickfix.SocketAcceptor(application, storeFactory, settings, logFactory)
+			acceptor.start()
+			# while condition == true: do something
+			acceptor.stop()
+	except quickfix.ConfigError, e:
+			print e
